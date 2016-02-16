9a8ff3d4fa2902aba40a0012cfb652cc958f7c32
Dorrin <adrian.fernandez.1@etu.u-bordeaux.fr>
2016-02-16 11:41:25 +0100

    uptade game.c

--- source/game.c
+++ source/game.c
@@ -1,6 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
+
+game new_game_hr (int nb_pieces, piece *pieces){
+    return NULL;
+}
+
+void delete_game (game g){
+}
+
+int game_nb_pieces(cgame g){
+    return 0;
+}
+
+cpiece game_piece(cgame g, int piece_num){
+    return NULL;
+}
+
+bool game_over_hr(cgame g){
+    return true;
+}
+
+bool play_move(game g, int piece_num, dir d, int distance){
+    return true;
+}
+
+int game_nb_moves(cgame g){
+    return 0;
+}
