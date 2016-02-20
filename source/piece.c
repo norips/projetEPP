e1becc0fee63f949aecbfc982730a9d35165d0c9
Domenger Amélie <amelie.domenger@etu.u-bordeaux.fr>
2016-02-20 10:50:36 +0100

    upDate piece.c

--- source/piece.c
+++ source/piece.c
@@ -11,8 +11,8 @@ struct position{
 
 struct piece_s{
   struct position position;
-  bool small;
-  bool horizontal;
+  bool isSmall;
+  bool isHorizontal;
   
 };
 
@@ -20,30 +20,35 @@ piece new_piece_rh (int x, int y, bool small, bool horizontal){
   piece newp = malloc(sizeof(struct piece_s));
   newp->position.x=x;
   newp->position.y=y;
-  newp->small = small;
-  newp->horizontal = horizontal;
+  newp->isSmall = small;
+  newp->isHorizontal = horizontal;
   return newp;
 }
 
 void delete_piece(piece p){
-  p->position.x=0;
-  p->position.y=0;
-  p->small = false;
-  p->horizontal = false;
   free(p);
 }
 
 void copy_piece(cpiece src, piece dst){
   dst->position.x=src->position.x;
   dst->position.y=src->position.y;
-  dst->small = src->small;
-  dst->horizontal = src->horizontal;
+  dst->isSmall = src->isSmall;
+  dst->isHorizontal = src->isHorizontal;
   
 }
 
 void move_piece(piece p, dir d, int distance){
-
-      
+  if(p->isHorizontal){
+    if (d == LEFT)
+      p->position.x -= distance;
+    if (d == RIGHT)
+      p->position.x += distance;
+  }else{
+    if (d == UP)
+      p->position.y += distance;
+    if (d == DOWN)
+      p->position.y -= distance;
+  }   
 }
 
 bool intersect(cpiece p1, cpiece p2){
