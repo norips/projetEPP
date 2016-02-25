b7d7f633f3adf6983defae05902bddc1169f1dfd
silvermoon <raphdruon@gmail.com>
2016-02-25 17:46:43 +0100

    Add gui and implement game

--- source/game.c
+++ source/game.c
@@ -29,26 +29,52 @@ void delete_game (game g){
 }
 
 void copy_game (cgame src, game dst){
-    
+    dst->arrPieces = malloc(sizeof(piece) * src->nbPieces);
+    for(int i=0;i<src->nbPieces;i++){
+        dst->arrPieces[i] = new_piece_rh(0, 0,false,false); //Create new piece
+        copy_piece(src->arrPieces[i],dst->arrPieces[i]); //Overwrite new piece with the copy
+    }
+    dst->nbPieces = src->nbPieces;
+    dst->nbMove = src->nbMove;
 }
 
 
 int game_nb_pieces(cgame g){
-    return 0;
+    return g->nbPieces;
 }
 
 cpiece game_piece(cgame g, int piece_num){
-    return NULL;
+    return g->arrPieces[piece_num];
 }
 
 bool game_over_hr(cgame g){
-    return true;
+    return get_x(g->arrPieces[0]) == 4 && get_y(g->arrPieces[0]);
 }
 
 bool play_move(game g, int piece_num, dir d, int distance){
+    piece tmp = new_piece_rh(0, 0,false,false);
+    copy_piece(game_piece(g,piece_num),tmp);
+    move_piece(tmp,d,distance);
+    if(get_x(tmp) == get_x(game_piece(g,piece_num)) && get_y(tmp) == get_y(game_piece(g,piece_num)))
+        return false;
+    if(d == LEFT || d == RIGHT){
+        if((get_x(tmp) + get_width(tmp)) > 6 || get_x(tmp) < 0)
+            return false;
+    } else {
+        if((get_y(tmp) + get_height(tmp)) > 6 || get_y(tmp) < 0)
+            return false;
+    }
+    for(int i=0;i<game_nb_pieces(g);i++){
+        if(i==piece_num)
+            continue;
+        if(intersect(tmp,game_piece(g,i)))
+            return false;
+    }
+    g->arrPieces[piece_num] = tmp;  
+    g->nbMove+=distance;
     return true;
 }
 
 int game_nb_moves(cgame g){
-    return 0;
+    return g->nbMove;
 }
--- source/poc_gui.c
+++ source/poc_gui.c
@@ -1,75 +1,118 @@
 
 #include <ncurses.h>
+#include <stdlib.h>
 #include <string.h>
+#include <game.h>
 #define SIZE 8
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 WINDOW *create_newcar(int starty, int startx,bool small,bool horizontal,int number);
+WINDOW *create_newscore(int height,int width,int starty, int startx,int score,bool playing);
 void destroy_win(WINDOW *local_win);
 
 int main(int argc, char *argv[])
-{	WINDOW *my_win,*my_car;
-	int startx, starty, width, height;
-	int ch;
+{	
+    WINDOW *my_win,*score;
+    WINDOW **car;
+    game newGame;
+    piece pieces[4];
+    pieces[0] = new_piece_rh(3, 3, true, true);
+    pieces[1] = new_piece_rh(3, 0, true, false);
+    pieces[2] = new_piece_rh(4, 1, true, true);
+    pieces[3] = new_piece_rh(5, 3, false, false);
+    newGame = new_game_hr(4,pieces);
+    car = malloc(sizeof(WINDOW*)*4);
+    int startx, starty, width, height;
+    int ch,carch,dir,dist;
 
-	initscr();			/* Start curses mode 		*/
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	noecho();
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
-        start_color();
-        use_default_colors();
-        init_pair(1, COLOR_RED, -1);
-	height = (3*SIZE)+1;
-	width = (3*SIZE)*2+1;
-	starty = 0;	/* Calculating for a center placement */
-	startx = 0;	/* of the window		*/
-	printw("Press 'q' to exit");
-	refresh(); 
+    initscr();			/* Start curses mode 		*/
+    cbreak();			/* Line buffering disabled, Pass on
+                                     * everty thing to me 		*/
+    noecho();
+    keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+    start_color();
+    use_default_colors();
+    init_pair(1, COLOR_RED, -1);
+    height = (3*SIZE)+1;
+    width = (3*SIZE)*2+1;
+    starty = 0;	/* Calculating for a center placement */
+    startx = 0;	/* of the window		*/
+    printw("Press 'q' to exit");
+    refresh(); 
+
+    int row,col;
+    getmaxyx(stdscr,row,col);
+    char mesg[]="Please resize to more than 50x25 actual";
+    while((row < 25) || (col < 50)){  getmaxyx(stdscr,row,col); mvprintw(row/2,(col-strlen(mesg))/2,"%s %dx%d",mesg,col,row); refresh(); clear();}
+    my_win = create_newwin(height, width, starty, startx); // Create new grid
+    wmove(my_win,0,0);
+    for(int i=0;i<game_nb_pieces(newGame);i++)
+        car[i] = create_newcar(get_y(game_piece(newGame,i))*(SIZE/2)+1, (get_x(game_piece(newGame,i)))*SIZE+1, true,is_horizontal(game_piece(newGame,i)),i); // Create new car
+    score = create_newscore(height,10,starty+width, startx,game_nb_moves(newGame),game_over_hr(newGame));
+    bool small = true;
+    bool horizontal = true;
+    int i=0;
+    int j=0;
+    while(ch != 'q')
+    {	
         
-        int row,col;
-        getmaxyx(stdscr,row,col);
-        char mesg[]="Please resize to more than 50x25 actual";
-        while((row < 25) || (col < 50)){  getmaxyx(stdscr,row,col); mvprintw(row/2,(col-strlen(mesg))/2,"%s %dx%d",mesg,col,row); refresh(); clear();}
-	my_win = create_newwin(height, width, starty, startx); // Create new grid
+//        switch(ch){
+//            case KEY_LEFT:
+//                i--;
+//                break;
+//            case KEY_RIGHT:
+//                i++;
+//                break;
+//            case KEY_UP:
+//                j--;
+//                break;
+//            case KEY_DOWN:
+//                j++;
+//                break;
+//            case 'c':
+//                small = !small;
+//                break;
+//            case 'v':
+//                horizontal = !horizontal;
+//
+//        }
+        mvprintw(height,0,"Please choose car :");
+        carch = getch();
+        mvprintw(height+1,0,"Please choose dest :");
+        dir = getch();
+        mvprintw(height+1,0,"Please choose dist :");
+        dist = getch();
+        switch(dir){
+            case KEY_LEFT:
+                play_move(newGame,carch-'0',LEFT,dist-'0');
+                break;
+            case KEY_RIGHT:
+                play_move(newGame,carch-'0',RIGHT,dist-'0');
+                break;
+            case KEY_DOWN:
+                play_move(newGame,carch-'0',UP,dist-'0');
+                break;
+            case KEY_UP:
+                play_move(newGame,carch-'0',DOWN,dist-'0');
+                break;
+        }
+        destroy_win(my_win);
+        for(int i=0;i<game_nb_pieces(newGame);i++)
+            destroy_win(car[i]);
+        destroy_win(score);
+        my_win = create_newwin(height, width, starty,startx);
         wmove(my_win,0,0);
-        my_car = create_newcar(1, 1, false,false,0); // Create new car
-        bool small = true;
-        bool horizontal = true;
-        int i=0;
-        int j=0;
-	while((ch = getch()) != 'q')
-	{	
-            switch(ch){
-                case KEY_LEFT:
-                    i--;
-                    break;
-                case KEY_RIGHT:
-                    i++;
-                    break;
-                case KEY_UP:
-                    j--;
-                    break;
-                case KEY_DOWN:
-                    j++;
-                    break;
-                case 'c':
-                    small = !small;
-                    break;
-                case 'v':
-                    horizontal = !horizontal;
-                    
-            }
-            destroy_win(my_win);
-            destroy_win(my_car);
-            my_win = create_newwin(height, width, starty,startx);
-            wmove(my_win,0,0);
-            my_car = create_newcar(j*(SIZE/2)+1, i*(SIZE)+1, small,horizontal,0); //Watch out, x and y are inversed
-            
-	}
-		
-	endwin();			/* End curses mode		  */
-	return 0;
+        for(int i=0;i<game_nb_pieces(newGame);i++)
+            car[i] = create_newcar(get_y(game_piece(newGame,i))*(SIZE/2)+1, get_x(game_piece(newGame,i))*SIZE+1,true,is_horizontal(game_piece(newGame,i)),i); // Create new car
+        wmove(my_win,0,0);
+        score = create_newscore(height,10,starty+width, startx,game_nb_moves(newGame),game_over_hr(newGame));
+        wmove(my_win,0,0);
+        
+
+    }
+
+    endwin();			/* End curses mode		  */
+    return 0;
 }
 
 WINDOW *create_newwin(int height, int width, int starty, int startx)
@@ -88,6 +131,13 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
             wvline(local_win,0,(SIZE*3)-1);
         }
         wmove(local_win,0,0);
+        wmove(local_win,(SIZE/2)*3,(SIZE)*6);
+        wattron(local_win,COLOR_PAIR(1)); //COLOR init with init_pair
+        wattron(local_win,A_BOLD); //Bold char
+        wvline(local_win,0,SIZE/2);
+        wattroff(local_win,A_BOLD);
+        wattroff(local_win,COLOR_PAIR(1));
+        
 	wrefresh(local_win);		/* Show that box 		*/
 
 	return local_win;
@@ -124,6 +174,24 @@ WINDOW *create_newcar(int starty, int startx,bool small,bool horizontal,int numb
     wattroff(local_win,A_BOLD);
     wattroff(local_win,COLOR_PAIR(1));
     wmove(local_win,0,0); //Set cur pos to top left car
+    wrefresh(local_win);		/* Show that box 		*/
+
+    return local_win;
+}
+
+WINDOW *create_newscore(int height,int width,int starty, int startx,int score,bool playing)
+{	
+    WINDOW *local_win;
+    
+    local_win = newwin(height, width, starty, startx);
+    wborder(local_win, '|', '|', '-','-','+','+','+','+');		/* 0, 0 gives default characters 
+                                                                    * for the vertical and horizontal
+                                             * lines			*/
+    char msg[] = "Status : ";
+    char msg2[] = "Score : ";
+    wmove(local_win,0,0); //Set cur pos to top left car
+    mvwprintw(local_win,0,0,"%s%s",msg,playing ? "Playing" : "Game Over");
+    mvwprintw(local_win,height/2,(width-(strlen(msg2)+1))/2,"%s%d",msg2,score);
     wrefresh(local_win);		/* Show that box 		*/
 
     return local_win;
